67d2350d84511fef88064f887582dfa1c64c8ecb
Justin Blalock <justin.m.blalock@gmail.com>
2021-11-16 21:19:11 -0600

    adding webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
-module.exports = () => ({
-  output: {
-    filename: "bundle.js",
-  },
-});
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = () => {
+  return {
+    output: {
+      filename: "bundle.js",
+    },
+    plugins: [new HtmlWebPackPlugin(), new webpack.ProgressPlugin()],
+  };
+};
