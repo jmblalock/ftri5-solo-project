4cf10effecdfe31816e7d9b878c9f9cef8d60c5f
Justin Blalock <justin.m.blalock@gmail.com>
2021-11-16 21:46:47 -0600

    starting to code with webpack

--- src/button.js
+++ src/button.js
@@ -4,7 +4,12 @@
  * @returns {Element}
  */
 const makeButton = (buttonName) => {
-  return `Button: ${buttonName}`;
+  const buttonLabel = `Button: ${buttonName}`;
+
+  const button = document.createElement("button");
+  button.innerText = buttonLabel;
+
+  return button;
 };
 
 export default makeButton;
