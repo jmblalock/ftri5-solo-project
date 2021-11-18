ae76da7d0c2dc1305da6102139394f54722daaa5
Justin Blalock <justin.m.blalock@gmail.com>
2021-11-17 18:17:42 -0600

    running webpack from npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
-module.exports = () => {
-  return {
-    entry: "./js/ClientApp.jsx",
-    output: {
-      filename: "bundle.js",
-    },
-    plugins: [new HtmlWebPackPlugin(), new webpack.ProgressPlugin()],
-  };
+module.exports = {
+  context: __dirname,
+  entry: "./js/ClientApp.jsx",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [new HtmlWebPackPlugin(), new webpack.ProgressPlugin()],
 };
